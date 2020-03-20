aa7d4ab5b3b0c22c0f9044d7c163de240bb24a95
andrewnoyes <andrew@noyes.io>
2020-03-20 16:34:51 -0600

    Support for changelog and prompt on version upgrade

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,10 +3,48 @@ const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const { kebabCase } = require('lodash');
 const yosay = require('yosay');
+const cmp = require('semver-compare');
+
+const generatorVersion = require('../../package.json').version;
+const releases = require('../../changelog.json').releases;
+const sortedVersions = Object.keys(releases).sort(cmp);
+
+const isVersionOld = (version) => version && cmp(version, generatorVersion) < 0;
+
+const getChangelog = (projectVersion) => {
+  const initialIndex = sortedVersions.indexOf(projectVersion);
+  const sliceStart = initialIndex >= 0 ? initialIndex : 0;
+  const releaseList = sortedVersions
+    .slice(sliceStart)
+    .reverse() // Display the most recent changes first
+    .map((version) => `[${version}]\n${(releases[version] || []).join('\n')}\n`)
+    .join('\n');
+
+  return `\n~~Changelog~~\n${releaseList}`;
+};
 
 module.exports = class extends Generator {
   async prompting() {
     this.log(yosay(`Welcome to the marvelous ${chalk.red('protect-integration')} generator!`));
+
+    const version = this.config.get('version');
+    if (isVersionOld(version)) {
+      this.log(getChangelog(version));
+
+      const confirm = await this.prompt([
+        {
+          type: 'confirm',
+          name: 'continue',
+          message: `Generator version: ${generatorVersion}, project version: ${version}. Are you sure you want to continue?`,
+        },
+      ]);
+
+      this.cancel = !confirm.continue;
+
+      if (this.cancel) {
+        return;
+      }
+    }
 
     const prompts = [
       {
@@ -25,7 +63,7 @@ module.exports = class extends Generator {
     ];
 
     this.answers = await this.prompt(prompts);
-    this.config.set(this.answers);
+    this.config.set({ ...this.answers, version: generatorVersion });
 
     this.composeWith(require.resolve('../switchboard'), this.answers);
     if (this.answers.includePhp) {
@@ -34,6 +72,10 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    if (this.cancel) {
+      return;
+    }
+
     this.config.save();
     this.fs.copyTpl(this.templatePath('./.*'), this.destinationPath('./'), this.answers);
     this.fs.copyTpl(this.templatePath('./*.*'), this.destinationPath('./'), this.answers);
