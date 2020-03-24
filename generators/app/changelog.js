d20a0815b1480e0428b227660819dbe3cad78be4
andrewnoyes <andrew@noyes.io>
2020-03-24 10:12:11 -0600

    Summary comments and changelog notes in docs

--- generators/app/changelog.js
+++ generators/app/changelog.js
@@ -1,16 +1,34 @@
 const semverCmp = require('semver-compare');
 
+/**
+ * Simple utility class to generate a changelog between current generator version and supplied version.
+ */
 module.exports = class Changelog {
+  /**
+   * Creates a changelog.
+   * @param {string} generatorVersion - Current version of the generator
+   * @param {{[string]: string[]}} releases - Object with the version as the key and an array of strings as value
+   */
   constructor(generatorVersion, releases) {
     this.generatorVersion = generatorVersion;
     this.releases = releases;
     this.sortedVersions = Object.keys(releases).sort(semverCmp);
   }
 
+  /**
+   * Returns true if the version is older than the generator version, false otherwise.
+   * @param {string} version - Version to compare against generator version
+   * @return {boolean} Whether the version is old.
+   */
   isVersionOld(version) {
     return version && semverCmp(version, this.generatorVersion) < 0;
   }
 
+  /**
+   * Gets a stringified changelog for display.
+   * @param {string} version - Version to generate changelog up to
+   * @return {string} Stringified changelog.
+   */
   getChangelog(version) {
     const initialIndex = this.sortedVersions.indexOf(version);
     const sliceStart = initialIndex >= 0 ? initialIndex : 0;
--- public/en/platform/generator-protect-integration/getting-started.md
+++ public/en/platform/generator-protect-integration/getting-started.md
@@ -17,3 +17,14 @@ npm link
 
 That will install your project dependencies and symlink a global module to your local file. After npm is done, you’ll be able to call yo name and you should see the this.log, defined earlier, rendered in the terminal. Congratulations, you just built your first generator!
 
+## Changelog
+
+**Developers**: for any breaking or backwards incompatible changes made to the generator, please record the version and one or more comments of what was changed into the [changelog.json](../../../../changelog.json). Add in your version as another key under `releases` and enter your comments as an array of strings. E.g:
+
+```json
+"releases": {
+  "0.0.13": [
+    "Support for a changelog, the key is the version and the value is an array of strings indicating what has changed"
+  ]
+}
+```
