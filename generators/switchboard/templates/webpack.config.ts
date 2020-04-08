862dbd3b9954e806860df50cba0b46b5fc54a954
Christopher Froehlich <christopher.r.froehlich@gmail.com>
2020-04-07 22:46:37 -0400

    Fix standard scaffold for webpack.

--- generators/switchboard/templates/webpack.config.ts
+++ generators/switchboard/templates/webpack.config.ts
@@ -3,7 +3,9 @@ import { BundleMode, BundleTarget, getWebpackConfig } from '@ns8/protect-tools-b
 export default getWebpackConfig({
   bundleTarget: BundleTarget.NODE,
   distDirectory: './dist',
-  sourceDirectory: './src',
+  // Globals hack to address https://github.com/node-formidable/formidable/issues/337#issuecomment-183388869
+  globals: [{ name: 'GENTLY', value: false }],
   libraryName: 'index',
   mode: BundleMode.DEVELOPMENT,
+    sourceDirectory: './src',
 });
